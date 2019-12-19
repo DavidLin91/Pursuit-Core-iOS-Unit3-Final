e694710d79721774a9a607c4ce332379e254f986
David <lin.david985@gmail.com>
2019-12-19 10:49:56 -0500

    Added model and network services

---
+++ Elements/Model/Element.swift
@@ -0,0 +1,20 @@
+//
+//  Element.swift
+//  Elements
+//
+//  Created by David Lin on 12/19/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+struct AllElements: Codable {
+    let name: String
+    let symbol: String
+    let atomicWeight: Double
+    let number: Int
+    let density: Double
+    let melt: Double
+    let boil: Double
+    let discovered_by: String
+}
---
+++ Elements/Network Services/AppError.swift
@@ -0,0 +1,39 @@
+//
+//  AppError.swift
+//  Elements
+//
+//  Created by David Lin on 12/19/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+enum AppError: Error, CustomStringConvertible {
+  case badURL(String) // associated value
+  case noResponse
+  case networkClientError(Error) // no internet connection
+  case noData
+  case decodingError(Error)
+  case encodingError(Error)
+  case badStatusCode(Int) // 404, 500
+  case badMimeType(String) // image/jpg
+  
+  // TODO: handle more descriptive language for
+  // error cases
+  var description: String {
+    switch self {
+    case .decodingError(let error):
+      return "\(error)"
+    case .badStatusCode(let code):
+      return "\(code)"
+    case .encodingError(let error):
+      return "encoding error: \(error)"
+    case .networkClientError(let error):
+      return "network error: \(error)"
+    case .badURL(let url):
+      return "Verify the url \(url)"
+    default:
+      return "other appError \(self)"
+    }
+  }
+}
